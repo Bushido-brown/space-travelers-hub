a7bca8b6666b598196e38fc3bba48df3d8c07231
jevazquezb <je.vazquezb@gmail.com>
2022-07-18 15:14:55 -0500

    Fix linters bugs

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 function NotFound() {
   return (
     <div>
-      <h1>Welcome to Space Tavellers' Hub!</h1>
+      <h1>Welcome to Space Tavellers&apos; Hub!</h1>
     </div>
   );
 }
